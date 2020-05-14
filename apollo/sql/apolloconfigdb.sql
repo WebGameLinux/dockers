9d8e7b2e3dfaab79fa25ff32812d1cc9fef3957e
nuokwan <994685563@qq.com>
2020-05-14 10:44:54 +0800

    add envs

--- apollo/sql/apolloconfigdb.sql
+++ apollo/sql/apolloconfigdb.sql
@@ -384,7 +384,7 @@ CREATE TABLE `AccessKey` (
 # ------------------------------------------------------------
 INSERT INTO `ServerConfig` (`Key`, `Cluster`, `Value`, `Comment`)
 VALUES
-    ('eureka.service.url', 'default', 'http://localhost:8080/eureka/,http://eureka.word-server.com/', 'Eureka服务Url，多个service以英文逗号分隔'),
+    ('eureka.service.url', 'default', 'http://localhost:8080/eureka/,http://eureka.word-server.cn/', 'Eureka服务Url，多个service以英文逗号分隔'),
     ('namespace.lock.switch', 'default', 'false', '一次发布只能有一个人修改开关'),
     ('item.value.length.limit', 'default', '20000', 'item value最大长度限制'),
     ('config-service.cache.enabled', 'default', 'false', 'ConfigService是否开启缓存，开启后能提高性能，但是会增大内存消耗！'),
